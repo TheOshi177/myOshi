58f26d45f817815106ff10be91d09590eccd4836
Oshi177 <Hubertos177@wp.pl>
2025-11-02 15:43:09 +0100

    1.075

--- Z5/myOshi/delete.php
+++ Z5/myOshi/delete.php
@@ -6,10 +6,28 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 }
 
 $user = preg_replace('/[^A-Za-z0-9_\-]/', '', $_SESSION['username']);
-$file = basename($_GET['file']);
-$path = __DIR__ . '/users/' . $user . '/' . $file;
+$target = basename($_GET['file']); // może być folder
+$path = __DIR__ . '/users/' . $user . '/' . $target;
 
-if (is_file($path)) unlink($path);
+// funkcja rekurencyjnie usuwa folder
+function deleteDir($dir) {
+    if (!file_exists($dir)) return;
+    if (is_file($dir) || is_link($dir)) {
+        unlink($dir);
+        return;
+    }
+    foreach (scandir($dir) as $item) {
+        if ($item === '.' || $item === '..') continue;
+        deleteDir($dir . DIRECTORY_SEPARATOR . $item);
+    }
+    rmdir($dir);
+}
+
+// usuń folder lub plik
+if (file_exists($path)) {
+    deleteDir($path);
+}
 
 header('Location: mycloud.php');
+exit;
 ?>
